cb134b84d39ce9694e92d270ae92ee5e95c65cba
Fred Pouchan <frederic.pouchan@gmail.com>
2020-11-04 10:49:34 +0100

    Grid en place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-nav></app-nav><router-outlet></router-outlet>'
+  template: '<link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@900&display=swap" rel="stylesheet"> <app-nav></app-nav><router-outlet></router-outlet>'
 })
 export class AppComponent {
   title = 'ecotraj';
