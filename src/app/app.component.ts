a0fcd8ca0b305a2b791465236795b5e7d8cc1c4c
Nicolas Galy <ngaly@hotmail.fr>
2020-11-05 10:49:23 +0100

    tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@900&display=swap" rel="stylesheet"> <app-nav></app-nav><router-outlet></router-outlet><app-footer></app-footer>'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'ecotraj';
